d60ac77c9833bfbe31d5f63086c5a63886505fdd
Ângelo Lucas <angelo.lrc@gmail.com>
2018-06-13 10:36:13 -0300

    Book shelf as component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import { Route } from 'react-router-dom'
 import './App.css'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
-
 
 class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
         <Route path="/search" component={SearchBooks} />
-        <Route exact path="/" component={ListBooks}/>
+        <Route exact path="/" component={ListBooks} />
       </div>
     )
   }
