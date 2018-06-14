8224c8934863d9abb04483bc3ae930899250eada
Ângelo Lucas <angelo.lrc@gmail.com>
2018-06-14 16:49:05 -0300

    Change book state from ListBooks to App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import './App.css'
+import * as BooksAPI from './BooksAPI'
 import SearchBooks from './SearchBooks'
 import ListBooks from './ListBooks'
+import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    books: [],
+    ready: false
+  }
+  componentDidMount() {
+    this.getBooks()
+  }
+  getBooks = () => {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        books,
+        ready: true
+      })
+    })
+  }
+  changeShelf = (bookId, e) => {
+    const shelf = e.target.value
+
+    BooksAPI.get(bookId).then((book) => {
+      BooksAPI.update(book, shelf)
+      this.getBooks()
+    })
+  }
   render() {
     return (
       <div className="app">
         <Route path="/search" component={SearchBooks} />
-        <Route exact path="/" component={ListBooks} />
+        {this.state.ready === true && (
+          <Route exact path="/" render={() => (
+            <ListBooks books={this.state.books} onChangeShelf={this.changeShelf} />
+          )} />
+        )}
       </div>
     )
   }
