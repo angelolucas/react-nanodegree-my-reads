d1cf76e5b04dd325c16d8a3138f3a11d7d5125c6
Ângelo Lucas <angelo.lrc@gmail.com>
2018-06-14 21:00:41 -0300

    Search treatment without result

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -5,18 +5,31 @@ import BookSummaryCards from './BookSummaryCards';
 
 class SearchBooks extends React.Component {
   state = {
-    query: '',
-    books: []
+    books: [],
+    status: false
   }
   componentDidMount() {
     this.searchInput.focus();
   }
   handlerSearch = (e) => {
-    BooksAPI.search(e.target.value).then((books) => {
-      this.setState({
-        books
+    if (e.target.value.length > 0) {
+      BooksAPI.search(e.target.value).then((books) => {
+        if (books.error === "empty query") {
+          this.setState({
+            status: 'noResult'
+          })
+        } else {
+          this.setState({
+            books,
+            status: 'result'
+          })
+        }
       })
-    })
+    } else {
+      this.setState({
+        status: ''
+      })
+    }
   }
   render() {
     return (
@@ -33,7 +46,15 @@ class SearchBooks extends React.Component {
           </div>
         </div>
         <div className="search-books-results">
-          <BookSummaryCards books={this.state.books} />
+          { this.state.status === "result" &&
+            <BookSummaryCards books={this.state.books} />
+          }
+          { this.state.status === "noResult" &&
+            <div>
+              <h2>No Results</h2>
+              <p><strong>Suggestions:</strong> Android, Art, Artificial Intelligence, Astronomy, Austen, Baseball, Basketball, Bhagat, Biography, Brief, Business, Camus, Cervantes, Christie, Classics, Comics, Cook, Cricket, Cycling, Desai, Design, Development, Digital Marketing, Drama, Drawing, Dumas, Education, Everything, Fantasy, Film, Finance, First, Fitness, Football, Future, Games, Gandhi, Homer, Horror, Hugo, Ibsen, Journey, Kafka, King, Lahiri, Larsson, Learn, Literary Fiction, Make, Manage, Marquez, Money, Mystery, Negotiate, Painting, Philosophy, Photography, Poetry, Production, Programming, React, Redux, River, Robotics, Rowling, Satire, Science Fiction, Shakespeare, Singh, Swimming, Tale, Thrun, Time, Tolstoy, Travel, Ultimate, Virtual Reality, Web Development, iOS</p>
+            </div>
+          }
         </div>
       </div>
     )
