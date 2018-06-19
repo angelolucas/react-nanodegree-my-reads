a4d84695f9685f9f89090fff74d9c84fd11c7bf1
Ângelo Lucas <angelo.lrc@gmail.com>
2018-06-19 14:41:15 -0300

    Style of Book Detail

--- src/book-details/index.js
+++ src/book-details/index.js
@@ -10,6 +10,7 @@ class BookDetail extends React.Component {
   selector = {
     body: document.querySelector('body')
   }
+
   componentDidMount = () => {
     const bookId = document.location.pathname.split("/book/").pop()
 
@@ -29,20 +30,24 @@ class BookDetail extends React.Component {
       <div className="book-details">
         <div className="book-details__top">
           <Link to="/" className="book-details__go-back">Back</Link>
-          <h1 className="book-details__title">{book.title}</h1>
+          <h1 className="book-details__title">
+            {book.title}
+          </h1>
         </div>
         <main className="book-details__content">
-          <h2>{book.subtitle}</h2>
-          <p>{book.authors}</p>
-          <p>{book.publishedDate}</p>
-          <p>{book.categories}</p>
-          <p>{book.pageCount} pages</p>
-          <p>{book.publisher}</p>
+          <h2 className="book-details__subtitle">{book.subtitle}</h2>
+          <ul className="book-details__info">
+            <li>{book.authors}</li>
+            <li>{book.publishedDate}</li>
+            <li>{book.categories}</li>
+            <li>{book.pageCount} pages</li>
+            <li>{book.publisher}</li>
+          </ul>
           {book.imageLinks && (
             <img className="book-details__cover" src={book.imageLinks.thumbnail} alt="Book cover" />
           )}
-          <p>{book.description}</p>
-          <a href={book.infoLink} target="_blank">see more</a>
+          <p className="book-details__description">{book.description}</p>
+          <a className="book-details__link" href={book.infoLink} target="_blank">See on Google Books</a>
         </main>
       </div>
     )
